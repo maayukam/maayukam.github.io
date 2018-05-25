0d588f62148ee64bc122c8393e189de2d7222594
vinothkumar <vinothkumar@vananservices.com>
2018-05-25 15:52:13 +0530

    testing

--- exambattle/models.py
+++ exambattle/models.py
@@ -1,4 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Signup(models.Model):
+    name = models.CharField(max_length=15,unique=True)
